210be27f9ff4fccb0e9a49163575bbcdd33d095d
Татьяна Абдулова <tanyas92@rambler.ru>
2022-03-09 11:20:50 +0500

    исправлено

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,24 @@ let popup = document.querySelector(".popup");
 let openPopup = document.querySelector(".profile__edit-button");
 let closePopup = popup.querySelector(".popup__button-recet");
 
-openPopup.addEventListener("click", function () {
-  popup.classList.toggle("popup__open");
-});
+function popupOpen() {
+  popup.classList.toggle("popup_open");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileInfo.textContent;
+};
+ 
+openPopup.addEventListener ("click", popupOpen);
 
-closePopup.addEventListener("click", function () {
-  popup.classList.toggle("popup__open");
-});
+function popupClose() {
+  popup.classList.toggle("popup_open");
+};
+
+closePopup.addEventListener("click", popupClose);
 
 // попап открытие и закрытие
 let formElement = document.querySelector(".form");
-let nameInput = document.querySelector(".form__name");
-let jobInput = document.querySelector(".form__info");
+let nameInput = document.querySelector(".form__container_name");
+let jobInput = document.querySelector(".form__container_info");
 let profileName = document.querySelector(".profile__title");
 let profileInfo = document.querySelector(".profile__subtitle");
 
@@ -21,14 +27,13 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
+  popup.classList.remove(popupClose);
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
 
 let closePopupp = popup.querySelector(".form__button");
 
-closePopupp.addEventListener("click", function () {
-  popup.classList.toggle("popup__open");
-});
+closePopupp.addEventListener("click", popupClose);
 
 //сохранение информации
